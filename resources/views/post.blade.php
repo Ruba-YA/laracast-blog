d86b7af8aaf4071ecf325a5218b6d219bf1d3ab2
Ruba <91896958+RubaUser@users.noreply.github.com>
2023-05-12 22:40:41 +0300

    edit posts view add content section

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-
-<title>My Blog!</title>
-<link rel="stylesheet" href="/app.css">
+@extends('layout')
 <body>
 
 <!-- <h1>Hello World !!</h1> -->
+@section('content')
 <article>
 <h1>
 {!! $post->title!!}</h1>
 <div>{!!$post->body!!}</div>
 <a href="/">GO Home!</a>
+</article>
+@endsection
