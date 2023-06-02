4a054fa275be816faa5b46cc7e06a173ee77c63d
Ruba <91896958+RubaUser@users.noreply.github.com>
2023-06-02 23:45:12 +0300

    adding posts-grid

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,50 +1,13 @@
-@section('content') 
-<!-- <h1>Hello World !!</h1> -->
-
-<!-- @foreach($posts as $post)
-<article>
-
-<h1>
-    
-<a href="/posts/{{$post->slug}}">
-
-{{$post->title}}
-<p></p>
-</a>
-<br>
-<p>
-By <a href="/authers/{{$post->auther->username}}">{{$post->auther->name}}</a> in <a href="/categories/{{$post->category->slug}}">{!!$post->Category->name!!}</a>
-</p>
-
-</h1>
-<div>
-
-{!! $post->excerpt !!}
-</div>
-</article>
-
-@endforeach
-@endsection -->
-
 <x-layout>
 @include('_post-header')
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-     <x-post-featured-card :post="$posts[0]" ></x-post-featured-card>
-            <!-- <div class="lg:grid lg:grid-cols-2">
-            <x-post-card></x-post-card>
-            <x-post-card></x-post-card>
-               
+              @if($posts->count())
+ <x-posts-grid :posts="$posts"></x-posts-grid>
+            @else
+            <h1 class="text-center">There is no posts</h1>
+            @endif
 
-              
-            </div>
-
-            <div class="lg:grid lg:grid-cols-3">
-                
-
-                   <x-post-card></x-post-card>
-                   <x-post-card></x-post-card>
-                   <x-post-card></x-post-card>
-            </div> -->
+          
         </main>
 
 </x-layout>
